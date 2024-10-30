dc89fb68de95f01302b28680914299e40af90731
oeddahab@emeal.nttdata.com <oeddahab@emeal.nttdata.com>
2024-10-30 11:20:03 +0100

    Este es el primer commit del taller practico 3 todavia no tiene ninguna actualización de código

---
+++ src/main/java/com/nttdata/nttdatacenters_spring_t2_OEDL/NttdatacentersSpringT2OedlApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacenters_spring_t2_OEDL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdatacentersSpringT2OedlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdatacentersSpringT2OedlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/nttdatacenters_spring_t2_OEDL/NttdatacentersSpringT2OedlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacenters_spring_t2_OEDL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdatacentersSpringT2OedlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
